baee9792780e00e3f1709bbcc63f80ee00aef2b4
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2024-09-06 20:47:55 +0530

    Add migration to update-channel-update-at function

---
+++ migrations/20240906151549-update-pgsql-function.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      CREATE OR REPLACE FUNCTION update_channel_last_message_timestamp()
+      RETURNS TRIGGER AS $$
+      BEGIN
+        -- Update the channel's updated_at with the new message's timestamp
+        UPDATE channels
+        SET updated_at = NEW.timestamp
+        WHERE id = NEW.channel_id::UUID;
+        RETURN NEW;
+      END;
+      $$ LANGUAGE plpgsql;
+    `);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      DROP FUNCTION IF EXISTS update_channel_last_message_timestamp;
+    `);
+  }
+};
