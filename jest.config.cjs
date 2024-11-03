87f174d2556993758481d051cc471af5fa212fe3
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2024-11-03 16:52:53 +0530

    chore: fix eslint configuration

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,21 @@
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   testEnvironmentOptions: {
-    NODE_ENV: "test"
+    NODE_ENV: 'test',
   },
   restoreMocks: true,
-  coveragePathIgnorePatterns: ["node_modules", "dist/config", "dist/app.js"],
-  coverageReporters: ["text", "lcov", "clover", "html"],
-  transform: { 
-    '\\.ts$': ['ts-jest', {
-      diagnostics: false
-    }]
-  }
+  coveragePathIgnorePatterns: ['node_modules', 'dist/config', 'dist/app.js'],
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  transform: {
+    '\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
 };
