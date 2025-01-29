a6a0aa62ff55aab2b9924d7eaf75040c2685bb14
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2025-01-29 23:20:45 +0530

    chore: setup docker for local backend setup

--- src/data/sequelize.ts
+++ src/data/sequelize.ts
@@ -1,11 +1,15 @@
-import { Sequelize } from "sequelize";
-import { PGDataSource } from "./data-sources/postgres/postgres-general-data-source";
-import { SequelizeDatabaseWrapper } from "./data-sources/postgres/sequelize-database-wrapper";
-import dotenv from "dotenv";
+import { Sequelize } from 'sequelize';
+import { PGDataSource } from './data-sources/postgres/postgres-general-data-source';
+import { SequelizeDatabaseWrapper } from './data-sources/postgres/sequelize-database-wrapper';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize: Sequelize = new Sequelize(process.env.DB_URL || 'postgresql://localhost:5432');
+// export const sequelize: Sequelize = new Sequelize(process.env.DB_URL || 'postgresql://localhost:5432');
+
+export const sequelize: Sequelize = new Sequelize(
+  process.env.DB_URL || 'postgresql://postgres:postgres@localhost:5433/showoff',
+);
 
 export async function connectPSQL() {
   const sequelizeDbWrapper = new SequelizeDatabaseWrapper(sequelize);
